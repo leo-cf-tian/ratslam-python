deace4e6c9727fc5f78d574fea4a738e1ce17f6f
Yash Joshi <rubiksprogrammer@gmail.com>
2025-12-01 00:45:55 -0500

    fix main script

--- main.py
+++ main.py
@@ -45,27 +45,34 @@ import ratslam
 
 if __name__ == '__main__':
     # Change this line to open other movies
-    data = r'D:\Bkp\ratslam\data\stlucia_testloop.avi'
+    data = r'./out.avi'
 
     video = cv2.VideoCapture(data)
     slam = ratslam.Ratslam()
     
+    DATASET_START = 120
+    
     loop = 0
     _, frame = video.read()
+    fps = video.get(cv2.CAP_PROP_FPS)  # for time calculation
     while True:
         loop += 1
+
 
         # RUN A RATSLAM ITERATION ==================================
         _, frame = video.read()
         if frame is None: break
+        
+        if loop % 30 != 0:
+            continue
 
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         slam.digest(img)
         # ==========================================================
 
         # Plot each 50 frames
-        if loop%50 != 0:
-            continue
+        # if loop%50 != 0:
+        #     continue
 
         # PLOT THE CURRENT RESULTS =================================
         b, g, r = cv2.split(frame)
@@ -77,6 +84,12 @@ if __name__ == '__main__':
         ax = plot.subplot(2, 2, 1)
         plot.title('RAW IMAGE')
         plot.imshow(rgb_frame)
+        
+        frame_num = int(video.get(cv2.CAP_PROP_POS_FRAMES)) - DATASET_START
+        time_sec = frame_num / fps
+        ax.text(10, 20, f'{time_sec}s', color='white', fontsize=12, 
+            bbox=dict(facecolor='black', alpha=0.5, pad=2))
+        
         ax.get_xaxis().set_ticks([])
         ax.get_yaxis().set_ticks([])
         # -----------------------------
@@ -86,40 +99,38 @@ if __name__ == '__main__':
         plot.title('RAW ODOMETRY')
         plot.plot(slam.odometry[0], slam.odometry[1])
         plot.plot(slam.odometry[0][-1], slam.odometry[1][-1], 'ko')
-        #------------------------------
+        # -----------------------------
 
         # POSE CELL ACTIVATION --------
         ax = plot.subplot(2, 2, 3, projection='3d')
         plot.title('POSE CELL ACTIVATION')
         x, y, th = slam.pc
         ax.plot(x, y, 'x')
-        ax.plot3D([0, 60], [y[-1], y[-1]], [th[-1], th[-1]], 'K')
-        ax.plot3D([x[-1], x[-1]], [0, 60], [th[-1], th[-1]], 'K')
-        ax.plot3D([x[-1], x[-1]], [y[-1], y[-1]], [0, 36], 'K')
-        ax.plot3D([x[-1]], [y[-1]], [th[-1]], 'mo')
+        ax.plot3D([0, 60], [y[-1], y[-1]], [th[-1], th[-1]], color='k')
+        ax.plot3D([x[-1], x[-1]], [0, 60], [th[-1], th[-1]], color='k')
+        ax.plot3D([x[-1], x[-1]], [y[-1], y[-1]], [0, 36], color='k')
+        ax.plot3D([x[-1]], [y[-1]], [th[-1]], color='m', marker='o')
         ax.grid()
-        ax.axis([0, 60, 0, 60]);
+        ax.set_xlim(0, 60)
+        ax.set_ylim(0, 60)
         ax.set_zlim(0, 36)
         # -----------------------------
 
         # EXPERIENCE MAP --------------
         plot.subplot(2, 2, 4)
         plot.title('EXPERIENCE MAP')
-        xs = []
-        ys = []
-        for exp in slam.experience_map.exps:
-            xs.append(exp.x_m)
-            ys.append(exp.y_m)
+        xs = [exp.x_m for exp in slam.experience_map.exps]
+        ys = [exp.y_m for exp in slam.experience_map.exps]
 
         plot.plot(xs, ys, 'bo')
         plot.plot(slam.experience_map.current_exp.x_m,
-                  slam.experience_map.current_exp.y_m, 'ko')
+                slam.experience_map.current_exp.y_m, 'ko')
         # -----------------------------
 
         plot.tight_layout()
-        # plot.savefig('C:\\Users\\Renato\\Desktop\\results\\forgif\\' + '%04d.jpg'%loop)
         plot.pause(0.1)
         # ==========================================================
+
 
     print('DONE!')
     print(('n_ templates:', len(slam.view_cells.cells)))
